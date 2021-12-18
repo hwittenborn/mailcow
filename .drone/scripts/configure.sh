4e2a045bc84fd6a2568659a6c383492edc9b22da
Hunter Wittenborn <hunter@hunterwittenborn.com>
2021-12-17 21:36:41 -0600

    Fixed duplicate '$' characters in variable references in CI configure script
    They were leftover from when the commands were directly in the Drone CI config.

--- .drone/scripts/configure.sh
+++ .drone/scripts/configure.sh
@@ -4,13 +4,13 @@ set -ex
 apt-get update
 apt-get install curl sed openssl docker-compose docker git gawk -y
 
-export MAILCOW_HOSTNAME="mailcow.$${hw_url}"
+export MAILCOW_HOSTNAME="mailcow.${hw_url}"
 export MAILCOW_TZ="$(cat /etc/timezone)"
 ./generate_config.sh
 
-sed -i "s|^DBPASS=.*|DBPASS=$${mailcow_db_user_password}|" mailcow.conf
-sed -i "s|^DBROOT=.*|DBROOT=$${mailcow_db_root_password}|" mailcow.conf
+sed -i "s|^DBPASS=.*|DBPASS=${mailcow_db_user_password}|" mailcow.conf
+sed -i "s|^DBROOT=.*|DBROOT=${mailcow_db_root_password}|" mailcow.conf
 sed -i "s|^HTTP_PORT=.*|HTTP_PORT=5070|" mailcow.conf
 sed -i "s|^HTTPS_PORT=.*|HTTPS_PORT=5071|" mailcow.conf
 sed -Ei "s;^(HTTP_BIND)=.*|^(HTTPS_BIND)=.*;\1=127.0.0.1;g" mailcow.conf
-sed -i "s|^ADDITIONAL_SAN=.*|ADDITIONAL_SAN=imap.$${hw_url},smtp.$${hw_url}|" mailcow.conf
+sed -i "s|^ADDITIONAL_SAN=.*|ADDITIONAL_SAN=imap.${hw_url},smtp.${hw_url}|" mailcow.conf
