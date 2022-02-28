714ec7184c3b2db6beb14f12b3c9d3885c404033
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-02-28 15:41:50 -0600

    Fixed port bindings in Mailcow config

--- .drone/scripts/configure.sh
+++ .drone/scripts/configure.sh
@@ -13,6 +13,7 @@ sed -i "s|^DBROOT=.*|DBROOT=${mailcow_db_root_password}|" mailcow.conf
 sed -i "s|^SQL_PORT=.*|SQL_PORT=5072|" mailcow.conf
 sed -i "s|^HTTP_PORT=.*|HTTP_PORT=5070|" mailcow.conf
 sed -i "s|^HTTPS_PORT=.*|HTTPS_PORT=5071|" mailcow.conf
-sed -Ei "s;^(HTTP_BIND)=.*|^(HTTPS_BIND)=.*;\1=127.0.0.1;g" mailcow.conf
+sed -i 's|^HTTP_BIND=.*|HTTP_BIND=127.0.0.1|' mailcow.conf
+sed -i 's|^HTTPS_BIND=.*|HTTPS_BIND=127.0.0.1|' mailcow.conf
 sed -i "s|^ADDITIONAL_SAN=.*|ADDITIONAL_SAN=imap.${hw_url},smtp.${hw_url}|" mailcow.conf
 sed -i "s|^SKIP_LETS_ENCRYPT=.*|SKIP_LETS_ENCRYPT=y|" mailcow.conf
