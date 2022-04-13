54108bbc991127875d35a566b8f8bfcc742bd1da
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-04-12 22:26:20 -0500

    Add server service file for deployments

--- .drone/scripts/deploy.sh
+++ .drone/scripts/deploy.sh
@@ -8,37 +8,19 @@ docker-compose down --remove-orphans
 cd -
 
 find "${deploy_path}" \
-     -maxdepth 1 \
-     -not -path "${deploy_path}" \
-     -not -path "${deploy_path}/service.sh" \
-     -not -path "${deploy_path}/data" \
-     -exec rm '{}' -rfv \;
-
-find "${deploy_path}/data" \
-     -maxdepth 1 \
-     -not -path "${deploy_path}/data" \
-     -not -path "${deploy_path}/data/user" \
-     -exec rm '{}' -rfv \;
+    -mindepth 1 \
+    -maxdepth 1 \
+    -exec rm '{}' -rfv \;
 
 find ./ \
-     -maxdepth 1 \
-     -not -path './' \
-     -not -path "./service.sh" \
-     -not -path "./data" \
-     -exec cp '{}' "${deploy_path}/{}" -Rv \;
-
-if ! [[ -d "${deploy_path}/data/" ]]; then
-	mkdir "${deploy_path}/data/"
-fi
-
-find ./data \
-     -maxdepth 1 \
-     -not -path "./data" \
-     -not -path "./data/user" \
-     -exec cp '{}' "${deploy_path}/{}" -Rv \;
+    -mindepth 1 \
+    -maxdepth 1 \
+    -exec cp '{}' "${deploy_path}/{}" -Rv \;
 
 cp /etc/letsencrypt/live/homelab/fullchain.pem "${deploy_path}/data/assets/ssl/cert.pem"
 cp /etc/letsencrypt/live/homelab/privkey.pem "${deploy_path}/data/assets/ssl/key.pem"
 
 cd "${deploy_path}"
 docker-compose up -d
+
+# vim: set sw=4 expandtab
---
+++ service.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+# Script managed by my fork to work with my server setup.
+set -e
+
+cd "$(dirname "${0}")"
+
+case "${1}" in
+    start)
+        docker-compose up -d
+        ;;
+    stop)
+        docker-compose down
+        ;;
+    backup-prepare)
+        docker-compose up -d
+        yes | BACKUP_LOCATION="${PWD}/backups" helper-scripts/backup_and_restore.sh backup all --delete-days 1
+        ;;
+    update)
+        docker-compose pull
+        ;;
+esac
+
+# vim: expandtab sw=4
